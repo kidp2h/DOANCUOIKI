154d87c896e5c9c68b11eb1218c424e3bf7c1f31
PhuThinh <phuthinh5322@gmail.com>
2021-12-31 14:49:22 +0700

    nghich

--- management/IManagement.java
+++ management/IManagement.java
@@ -3,7 +3,7 @@ package DOANCUOIKI.management;
 import java.util.List;
 
 public interface IManagement<T> {
-    public void LoadFile(String path);  
+    void LoadFile(String path);  
     List<T> GetList();
     void PrintList();
     void Add(T obj, String path);
--- management/PersonManagement.java
+++ management/PersonManagement.java
@@ -1,13 +1,11 @@
 package DOANCUOIKI.management;
 
-import java.io.*;
-import java.lang.System;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import DOANCUOIKI.Person;
-import DOANCUOIKI.util.Color;
 import DOANCUOIKI.util.RWFile;
 import DOANCUOIKI.ENV;
 
@@ -30,6 +28,7 @@ public class PersonManagement extends Management<Person> implements IManagement<
     list = new ArrayList<>();
     LoadFile(ENV.pathPerson);
   }
+  
   @Override
   public List<Person> SearchByName(String name) {
     return list.stream()
