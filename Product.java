6ea631573808c3568dfda8fe04f0323daf0a305b
KidP2H <40386174+kidp2h@users.noreply.github.com>
2021-12-31 19:51:05 +0700

    exists id product

--- Product.java
+++ Product.java
@@ -3,22 +3,22 @@ package DOANCUOIKI;
 import java.io.Serializable;
 
 public class Product extends Entity implements Serializable {
-  private int id;
+  private String id;
   public String name;
   public String category;
   public int price;
 
-  public Product(String name, String category, int price) {
-    this.id = 5;
+  public Product(String id, String name, String category, int price) {
+    this.id = id;
     this.name = name;
     this.category = category;
     this.price = price;
 }
 
-  public int getId() {
+  public String getId() {
     return id;
   }
-  public void setId(int id) {
+  public void setId(String id) {
     this.id = id;
   }
   public String getName() {
@@ -39,10 +39,12 @@ public class Product extends Entity implements Serializable {
   public void setPrice(int price) {
     this.price = price;
   }
+  
   @Override
   public String Info() {
-    return  
-    name + "\t"
+    return
+      id+"\t"
+    + name + "\t"
     + category + "\t"
     + price;
   }
